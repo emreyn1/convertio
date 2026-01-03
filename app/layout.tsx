83a0f638d3eb74235124049abe8581512b768440
Emre Hallac <arif-hallac@hotmail.com>
2026-01-03 20:51:46 +0400

    hydration sorunu cozuldu

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <GA GA_MEASUREMENT_ID="G-52GQ441X7H" />
             <meta name="google-site-verification" content="V8lmEvFOdYBlChgR6pYABBZBhI1EFnPb1YuxTTdHXMU" />
             <body className={inter.className}>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem themes={["light", "dark"]}>
+                <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} enableColorScheme={false} themes={["light", "dark"]}>
                     <Navbar />
                     <Toaster />
                     <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
